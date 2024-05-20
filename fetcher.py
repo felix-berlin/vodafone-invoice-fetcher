38a4aca0341bd5f8a82b04baf1b83ecae601677e
Felix Scholze <felix@webshaped.de>
2024-05-20 23:45:25 +0200

    feat: add remote url option

--- fetcher.py
+++ fetcher.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import argparse
 
@@ -15,20 +16,32 @@ parser.add_argument('--mode', type=str, default="last", help='The mode to fetch 
 parser.add_argument('--username', type=str, required=True, help='The username')
 parser.add_argument('--password', type=str, required=True, help='The password')
 parser.add_argument('--secret_key', type=str, required=False, help='The 2FA secret key')
-parser.add_argument('--download_path', type=str, default="./downloads", help='The download path for the invoices')
+parser.add_argument('--download_path', type=str, default=None, help='The download path for the invoices')
 parser.add_argument('--login_url', type=str, default="https://www.vodafone.de/meinvodafone/account/login", help='Vodafone login URL')
 parser.add_argument('--invoice_overview_url', type=str, default="https://www.vodafone.de/meinvodafone/services/ihre-rechnungen/rechnungen", help='Vodafone invoice overview URL')
+parser.add_argument('--remote_url', type=str, default=None, help='The URL of the remote Selenium server')
 
 # Parse the arguments
 args = parser.parse_args()
 
 # Initialize a WebDriver instance
 chrome_options = Options()
-chrome_options.add_experimental_option('prefs',  {
-    "download.default_directory": args.download_path, # Change this to your download directory
-})
 
-driver = webdriver.Chrome(options=chrome_options)
+if args.download_path:
+  chrome_options.add_experimental_option('prefs',  {
+      "download.default_directory": args.download_path, # Change this to your download directory
+  })
+
+# Set the capabilities
+capabilities = chrome_options.to_capabilities()
+
+if args.remote_url:
+    driver = webdriver.Remote(
+        command_executor=args.remote_url,
+        options=chrome_options
+    )
+else:
+    driver = webdriver.Chrome(options=chrome_options)
 
 def initVodafoneInvoiceFetcher(username, password, secret_key):
     # Open the login page
